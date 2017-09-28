c220a164652256faa74a3fdbe50ebe9a14f36792
harish-rzt <harishv6665@gmail.com>
2017-09-29 01:22:17 +0530

    Header, banner, catelogue

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
 var del = require('del');
 
 gulp.task('sass', function() {
@@ -17,6 +18,10 @@ gulp.task('sass', function() {
 
 gulp.task('clean', function() {
     return del(['dist']);
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/stylesheets/**/*.scss', ['sass'])
 });
 
 gulp.task('default', ['clean', 'sass']);
